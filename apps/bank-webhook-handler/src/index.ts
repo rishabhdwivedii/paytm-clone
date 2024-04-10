7799ed35601e9fca7655b9bde53ba6cba412b6c7
Rishabh Dwivedi <rd14121999@gmail.com>
2024-04-11 01:13:33 +0530

    added transaction if webhook-handler fails in middle

--- apps/bank-webhook-handler/src/index.ts
+++ apps/bank-webhook-handler/src/index.ts
@@ -10,24 +10,38 @@ app.post("/hdfcWebhook", async (req, res) => {
     userId: req.body.user_identifier,
     amount: req.body.amount,
   };
-  await db.balance.update({
-    where: {
-      userId: paymentInformation.userId,
-    },
-    data: {
-      amount: paymentInformation.amount,
-    },
-  });
 
-  await db.onRampTransaction.update({
-    where: {
-      token: paymentInformation.token,
-    },
-    data: {
-      status: "Success",
-    },
-  });
-  res.status(200).json({
-    message: "Captured",
-  });
+  try {
+    await db.$transaction([
+      db.balance.updateMany({
+        where: {
+          userId: Number(paymentInformation.userId),
+        },
+        data: {
+          amount: {
+            increment: Number(paymentInformation.amount),
+          },
+        },
+      }),
+      db.onRampTransaction.updateMany({
+        where: {
+          token: paymentInformation.token,
+        },
+        data: {
+          status: "Success",
+        },
+      }),
+    ]);
+
+    res.json({
+      message: "Captured",
+    });
+  } catch (e) {
+    console.error(e);
+    res.status(411).json({
+      message: "Error while processing webhook",
+    });
+  }
 });
+
+app.listen(3003);
